bb0ef5674b5b6455be79965ccd6db7a902432dec
PanGeun <lifebook0809@gmail.com>
2022-11-14 22:51:03 +0900

    [INIT] 프로젝트 생성
    - 프로젝트 생성 및 디펜던시 추가
    - oracle DB 암호화

---
+++ src/main/java/com/rence/RenceBootApplication.java
@@ -0,0 +1,14 @@
+package com.rence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenceBootApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RenceBootApplication.class, args);
+		
+	}
+	
+}
