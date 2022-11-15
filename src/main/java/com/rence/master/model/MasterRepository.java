d42841b02231911eeeea5e5e738c468bf76552f7
PanGeun <lifebook0809@gmail.com>
2022-11-15 10:59:53 +0900

    [feat] master controller 뼈대 생성

---
+++ src/main/java/com/rence/master/model/MasterRepository.java
@@ -0,0 +1,11 @@
+package com.rence.master.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface MasterRepository extends JpaRepository<MasterEntity, Object> {
+
+	
+	@Query(nativeQuery=true, value="SELECT * FROM MASTERINFO WHERE master_id=?1 AND master_pw=?2")
+	public MasterEntity findByMaster_id(String master_id, String master_pw);
+}
---
+++ src/main/java/com/rence/master/model/MasterVO.java
@@ -0,0 +1,17 @@
+package com.rence.master.model;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MasterVO implements Serializable {
+
+	private String master_no;
+	private String master_id;
+	private String master_pw;
+}
