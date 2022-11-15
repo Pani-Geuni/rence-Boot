cfe691c19dfd8592ea992d8a139296d4995c68da
Choi <choetruth44@gmail.com>
2022-11-16 00:13:09 +0900

    [feat] Master 세팅
    페이징, 로그인 제외 세팅 완료

--- src/main/java/com/rence/backoffice/service/BackOfficeRepository.java
+++ src/main/java/com/rence/backoffice/service/BackOfficeRepository.java
@@ -6,6 +6,7 @@
 package com.rence.backoffice.service;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +14,8 @@ import org.springframework.data.jpa.repository.Query;
 import com.rence.backoffice.model.BackOfficeVO;
 
 public interface BackOfficeRepository extends JpaRepository<BackOfficeVO, Object> {
+
+
 
 	@Query(nativeQuery = true, value="SELECT * from backofficeinfo where backoffice_email=?1 and backoffice_state !='X'")
 	public BackOfficeVO select_backoffice_no(String backoffice_email);
@@ -38,7 +41,24 @@ public interface BackOfficeRepository extends JpaRepository<BackOfficeVO, Object
 	public BackOfficeVO findByBackoffice_email(String backoffice_id); 
 
 
+	@Query(nativeQuery = true, value="select * from (select rownum as rnum, backoffice_no,TO_CHAR(apply_date, 'YYYY-MM-DD HH24:MI:SS') as apply_date,company_name,owner_name,backoffice_id,backoffice_name,backoffice_tel,backoffice_email from backofficeinfo where backoffice_state='W' order by apply_date desc) where rnum between ?1 and ?2")
+	public List<BackOfficeVO> selectAll_backoffice_apply(Integer start_row, Integer end_row);
 
+	@Query(nativeQuery = true, value="UPDATE backofficeinfo SET backoffice_state='Y' where backoffice_no=?1 and backoffice_email=?2")
+	public int update_backoffice_state_y(String backoffice_no, String backoffice_email);
+
+	@Query(nativeQuery = true, value="UPDATE backofficeinfo SET backoffice_state='N' where backoffice_no=?1 and backoffice_email=?2")
+	public int update_backoffice_state_N(String backoffice_no, String backoffice_email);
+
+	@Query(nativeQuery = true, value="select * from (select rownum as rnum,  backoffice_no,TO_CHAR(apply_date, 'YYYY-MM-DD HH24:MI:SS') as apply_date,company_name,owner_name,backoffice_id,backoffice_name,backoffice_tel,backoffice_email from backofficeinfo where backoffice_state='O' order by apply_date desc) where rnum between ?1 and ?2")
+	public List<BackOfficeVO> selectAll_backoffice_end(Integer start_row, Integer end_row);
+
+	@Query(nativeQuery = true, value="UPDATE backofficeinfo SET backoffice_state='X' where backoffice_no=?1 and backoffice_email=?2")
+	public int update_backoffice_state_X(String backoffice_no, String backoffice_email);
+
+	@Query(nativeQuery = true, value="SELECT * from backofficeinfo where backoffice_no=?1")
+	public BackOfficeVO selectOne_backoffice_detail_m(String backoffice_no);
+	
 	
 
 
--- src/main/java/com/rence/master/service/MasterService.java
+++ src/main/java/com/rence/master/service/MasterService.java
@@ -1,12 +1,17 @@
 /**
- * @author 전판근
+ * @author 전판근, 최진실
  */
 
 package com.rence.master.service;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rence.backoffice.model.BackOfficeVO;
+import com.rence.backoffice.service.BackOfficeRepository;
 import com.rence.master.model.MasterEntity;
 import com.rence.master.model.MasterRepository;
 import com.rence.master.model.MasterVO;
@@ -18,16 +23,90 @@ import lombok.extern.slf4j.Slf4j;
 public class MasterService {
 
 	@Autowired
-	MasterRepository repository;
-	
+	MasterRepository m_repository;
+
+	@Autowired
+	BackOfficeRepository b_repository;
+
 	public MasterService() {
 		log.info("MasterService()...");
 	}
-	
+
 	public MasterEntity master_login(MasterVO vo) {
-		
-		return repository.findByMaster_id(vo.getMaster_id(), vo.getMaster_pw());
+
+		return m_repository.findByMaster_id(vo.getMaster_id(), vo.getMaster_pw());
 	}
-	
-	
+
+	/*
+	 * 마스터 메인 화면 (호스트 신청 리스트)
+	 * 
+	 */
+	public long total_rowCount() {
+		log.info("total_rowCount()....");
+
+		return b_repository.count();
+	}
+
+	public List<BackOfficeVO> backoffice_applyList_selectAll(Integer currentPage) {
+		log.info("backoffice_applyList_selectAll().....");
+		log.info("currentpage:{}", currentPage);
+
+		Integer row_count = 5;
+		Integer start_row = (currentPage - 1) * row_count + 1;
+		Integer end_row = currentPage * row_count;
+
+		return b_repository.selectAll_backoffice_apply(start_row, end_row);
+	}
+
+	/*
+	 * 마스터 메인 화면 (호스트 신청 승인 처리)
+	 * 
+	 */
+	public int backoffice_grant(BackOfficeVO bvo) {
+		log.info("backoffice_grant().....");
+		return b_repository.update_backoffice_state_y(bvo.getBackoffice_no(),bvo.getBackoffice_email());
+	}
+
+	/*
+	 * 마스터 메인 화면 (호스트 신청 거절 처리)
+	 * 
+	 */
+	public int backoffice_refuse(BackOfficeVO bvo) {
+		log.info("backoffice_refuse().....");
+		return b_repository.update_backoffice_state_N(bvo.getBackoffice_no(),bvo.getBackoffice_email());
+	}
+
+	/*
+	 * 마스터 - 호스트 탈퇴 신청 리스트
+	 * 
+	 */
+	public List<BackOfficeVO> backoffice_endList_selectAll(Integer page) {
+		log.info("backoffice_applyList_selectAll().....");
+		log.info("currentpage:{}", page);
+
+		Integer row_count = 5;
+		Integer start_row = (page - 1) * row_count + 1;
+		Integer end_row = page * row_count;
+
+		return b_repository.selectAll_backoffice_end(start_row, end_row);
+	}
+
+	/*
+	 * 마스터 - 호스트 탈퇴 승인
+	 * 
+	 */
+	public int backoffice_revoke(BackOfficeVO bvo) {
+		log.info("backoffice_refuse().....");
+		return b_repository.update_backoffice_state_X(bvo.getBackoffice_no(),bvo.getBackoffice_email());
+	}
+
+	/*
+	 * 마스터 - 백오피스 상세
+	 * 
+	 */
+	public BackOfficeVO master_backoffice_detail_selectOne(BackOfficeVO bvo) {
+		log.info("backoffice_refuse().....");
+		return b_repository.selectOne_backoffice_detail_m(bvo.getBackoffice_no());
+	}
+
 }
