09d46031f6dae248ae7b0af9523145a579bf5ec4
wtw <qjspdl@hs.ac.kr>
2022-11-16 11:42:56 +0900

    [error] 오피스 에러 수정

--- src/main/java/com/rence/office/model/OfficeListRepository.java
+++ src/main/java/com/rence/office/model/OfficeListRepository.java
@@ -14,7 +14,7 @@ public interface OfficeListRepository extends JpaRepository<ListViewVO, Object> 
 			+ "select * from( "
 		+ "		    select left.backoffice_no, backoffice_type, company_name, roadname_address, backoffice_tag, REGEXP_SUBSTR(backoffice_image, '[^,]+', 1, 1) as backoffice_image, nvl(min_room_price, 0) as min_room_price, nvl(avg(rv.review_point), 0) as avg_rating from("
 		+ "		        select b.backoffice_no, b.backoffice_type, b.company_name, b.roadname_address, b.backoffice_tag, b.backoffice_image, min(r.room_price)as min_room_price "
-		+ "		        from backofficeinfo b left join roominfo "
+		+ "		        from backofficeinfo b left join roominfo r"
 		+ "		        on b.backoffice_no = r.backoffice_no "
 		+ "		        where b.backoffice_state = 'Y' group by b.backoffice_no, b.company_name, b.roadname_address, b.backoffice_tag, b.backoffice_image, b.backoffice_type "
 		+ "		    )left left join review rv "
@@ -26,16 +26,14 @@ public interface OfficeListRepository extends JpaRepository<ListViewVO, Object> 
 		+ "		) "
 		+ "		where backoffice_type like ?2 "
 		
-		+ "		IF ?1 == 'date' THEN "
-		+ "		ORDER BY backoffice_no desc"
-		+ "		IF ?1 == 'rating' THEN "
-		+ "		ORDER BY total.avg_rating desc, total.backoffice_no desc"
-		+ "		IF ?1 == 'cheap' THEN "
-		+ "		ORDER BY total.min_room_price, total.backoffice_no desc"
-		+ "		IF ?1 == 'expensive' THEN "
-		+ "		ORDER BY total.min_room_price desc, total.backoffice_no desc"
+		+ "		CASE ?1 WHEN 'date' THEN ORDER BY backoffice_no desc"
+		+ "             WHEN 'rating' THEN ORDER BY total.avg_rating desc, total.backoffice_no desc "
+		+ "             WHEN 'cheap' THEN ORDER BY total.min_room_price, total.backoffice_no desc "
+		+ "            WHEN 'expensive' THEN ORDER BY total.min_room_price desc, total.backoffice_no desc END"
 		+ "		)")
 	public List<ListViewVO> select_all_list(String condition, String type);
-	public List<ListViewVO> search_list(String type, String location, String searchWord, String condition);
+	
+	
+//	public List<ListViewVO> search_list(String type, String location, String searchWord, String condition);
 
 }
