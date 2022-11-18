24f6f7ab43e7edafec1d1d1d4c38c257bd02fd17
Choi <choetruth44@gmail.com>
2022-11-18 21:58:16 +0900

    [feat] DashBoard 작성자 추가
    DashBoard 작성자 추가

--- src/main/java/com/rence/dashboard/controller/DashBoardController.java
+++ src/main/java/com/rence/dashboard/controller/DashBoardController.java
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @author 최진실
+ *
+ */
 package com.rence.dashboard.controller;
 
 import java.util.ArrayList;
--- src/main/java/com/rence/dashboard/repository/ReserveSummaryRepository.java
+++ src/main/java/com/rence/dashboard/repository/ReserveSummaryRepository.java
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @author 최진실
+ *
+ */
 package com.rence.dashboard.repository;
 
 import java.util.List;
