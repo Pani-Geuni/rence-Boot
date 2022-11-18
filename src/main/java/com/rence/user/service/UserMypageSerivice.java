d9d3aa8c9ff056e0879fea1825be45c98f3209a1
KANGKYEONGSEOK <rudtjr1303@naver.com>
2022-11-18 23:33:15 +0900

    [refactor] 비밀번호 수정 암호화 및 비교 구현

--- src/main/java/com/rence/user/service/UserMypageSerivice.java
+++ src/main/java/com/rence/user/service/UserMypageSerivice.java
@@ -8,21 +8,30 @@ package com.rence.user.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.rence.user.model.MyPageReserveListVO;
+import com.rence.user.model.QuestionVO;
 import com.rence.user.model.UserMileageVO;
 import com.rence.user.model.UserMypageVO;
 import com.rence.user.model.UserVO;
 import com.rence.user.repository.MileageRepository;
+import com.rence.user.repository.MyQuestionRepository;
 import com.rence.user.repository.MyReserveRepository;
 import com.rence.user.repository.MypageRepository;
+import com.rence.user.repository.UserRepository;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 public class UserMypageSerivice {
+	
+	@Autowired
+	UserRepository userRepository;
 	
 	@Autowired
 	MypageRepository repository;
@@ -32,6 +41,9 @@ public class UserMypageSerivice {
 	
 	@Autowired
 	MyReserveRepository myReserveRepository;
+	
+	@Autowired
+	MyQuestionRepository myQuestionRepository;
 	
 	public UserMypageSerivice() {
 		log.info("UserSerivice().....");
@@ -81,23 +93,50 @@ public class UserMypageSerivice {
 		return null;
 		
 	}
+
+	
+	//현재 비밀번호 확인
+	public int check_now_pw(UserVO uvo) {
+		log.info("user_pw_updateOK()....");
+		log.info("(비밀번호 확인부분)uvo: {}",uvo);
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		//평문과 비교를 하는것이기때문에 입력된값(평문)을 암호화 해서는 안됨!!!!
+//		uvo.setUser_pw(encoder.encode(uvo.getPassword()));
+//		log.info("(비밀번호 확인부분)암호화 PW: {}",uvo.getUser_pw());
+//		log.info("(비밀번호 확인부분)암호화 uvo: {}",uvo);
+		
+		UserVO uvo2 = userRepository.check_now_pw_selectOne(uvo.getUser_no());
+		log.info("(비밀번호 확인부분)uvo2: {}",uvo2);
+		
+		boolean match_res = encoder.matches(uvo.getPassword(), uvo2.getPassword()); 
+		log.info("(비밀번호 확인부분)res: {}",match_res);
+		int result;
+		if(match_res == true) {
+			result = 1;
+		}else {
+			result = 0;
+		}
+			
+//		 uvo.setUser_pw("$2a$12$SkMR3vslquCDjRVoFWGtI.XoN8Bs8DsWfrYqHb.jSt6IL3EgXLVeC"); //test1234!	
+		return result;
+	}
+	
 	
 	//비밀번호 변경
 	public int user_pw_updateOK(UserVO uvo) {
 		log.info("user_pw_updateOK()....");
 		log.info("uvo: {}",uvo);
 		
+		//비밀번호 암호화 암호화
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		uvo.setUser_pw(encoder.encode(uvo.getUser_pw()));
 		
-		return repository.user_pw_updateOK(uvo.getUser_pw(),uvo.getUser_no());
-	}
-	
-	//현재 비밀번호 확인
-	public int check_now_pw(UserVO uvo) {
-		log.info("user_pw_updateOK()....");
-		log.info("uvo: {}",uvo);
+		log.info("(비밀번호 변경부분)암호화 PW: {}",uvo.getUser_pw());
 		
-		return repository.check_now_pw(uvo.getUser_no(),uvo.getUser_pw());
+		return userRepository.user_pw_updateOK(uvo.getUser_pw(),uvo.getUser_no());
 	}
+
 
 	//회원탈퇴에 따른 회원상태 수정
 	public int user_secedeOK(UserVO uvo) {
@@ -136,6 +175,20 @@ public class UserMypageSerivice {
 		log.info("vos: {}",vos);
 		
 		return vos;
+	}
+
+	//마이페이지 - 문의내역리스트
+	public List<QuestionVO> select_all_question(String user_no) {
+		log.info("select_all_question()....");
+		log.info("user_no: {}",user_no);
+		return myQuestionRepository.select_all_question(user_no);
+	}
+
+	//마이페이지 - 문의내역리스트
+	public QuestionVO select_one_answer(String comment_no) {
+		log.info("select_all_question()....");
+		log.info("comment_no: {}",comment_no);
+		return myQuestionRepository.select_one_answer(comment_no);
 	}
 
 
