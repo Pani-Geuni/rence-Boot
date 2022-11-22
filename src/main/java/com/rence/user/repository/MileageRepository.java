9c64b078efd5250eef5b11674dffbba7b151f10e
KANGKYEONGSEOK <rudtjr1303@naver.com>
2022-11-22 15:17:49 +0900

    [feat]마일리지리스트 페이징처리 구현

--- src/main/java/com/rence/user/repository/MileageRepository.java
+++ src/main/java/com/rence/user/repository/MileageRepository.java
@@ -10,26 +10,61 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.rence.user.model.UserMileageVO;
+import com.rence.user.model.UserVO;
 
 public interface MileageRepository extends JpaRepository<UserMileageVO, Object> {
 
 	// 총마일리지 조회
-	@Query(nativeQuery = true, 
-			value = "select * from( select * from USER_DETAIL_MILEAGE_VIEW where state !='W' and user_no=?1 order by no desc)WHERE ROWNUM between 1 and 1")
+	@Query(nativeQuery = true, value = "select * from( select * from USER_DETAIL_MILEAGE_VIEW where state !='W' and user_no=?1 order by no desc)WHERE ROWNUM between 1 and 1")
 	public UserMileageVO totalMileage_selectOne(String user_no);
 
-	// user_mileage_selectAll
-	@Query(nativeQuery = true, value = "select * from user_detail_mileage_view where user_no = ?1 order by no desc")
-	List<UserMileageVO> mileage_selectAll(String user_no);
+	// 마일리지 리스트수-전체(페이징 처리를 위해서) all
+	@Query(nativeQuery = true, value = "select count(*) ffrom user_detail_mileage_view where user_no = ?1")
+	public long count_allmileage(String user_no);
+	
+	// 마일리지 리스트수-적립(페이징 처리를 위해서) plus
+	@Query(nativeQuery = true, value = "select count(*) ffrom user_detail_mileage_view where user_no = ?1 and state='T'")
+	public long count_plusmileage(String user_no);
+	
+	// 마일리지 리스트수-사용(페이징 처리를 위해서) minus
+	@Query(nativeQuery = true, value = "select count(*) ffrom user_detail_mileage_view where user_no = ?1 and state='F'")
+	public long count_minusmileage(String user_no);
 
-	// user_mileage_search_list
-	@Query(nativeQuery = true, value = "select * from user_detail_mileage_view where user_no = ?1 order by no desc")
-	public List<UserMileageVO> mileage_search_list_all(String user_no);
+	
+	// 마일리지 리스트 all 페이징
+	@Query(nativeQuery = true, value = "select * from (select * from user_detail_mileage_view where user_no = ?1 order by no desc) where rownum between ?2 and ?3")
+	public List<UserMileageVO> user_mileage_selectAll_paging(String user_no, Integer start_row, Integer end_row);
+	
+	
+	
+	// user_mileage_search_list paging
+	@Query(nativeQuery = true, value = "select * from (select * from user_detail_mileage_view where user_no = ?1 and state='T' order by no desc) where rownum between ?2 and ?3")
+	public List<UserMileageVO> mileage_search_list_plus_paging(String user_no, Integer start_row, Integer end_row);
+	
+	@Query(nativeQuery = true, value = "select * from (select * from user_detail_mileage_view where user_no = ?1 and state='F' order by no desc) where rownum between ?2 and ?3")
+	public List<UserMileageVO> mileage_search_list_minus_paging(String user_no, Integer start_row, Integer end_row);
 
-	@Query(nativeQuery = true, value = "select * from user_detail_mileage_view where user_no = ?1 and state='T' order by no desc")
-	public List<UserMileageVO> mileage_search_list_plus(String user_no);
+	
+	
+//	// user_mileage_selectAll
+//	@Query(nativeQuery = true, value = "select * from user_detail_mileage_view where user_no = ?1 order by no desc")
+//	List<UserMileageVO> mileage_selectAll(String user_no);
+//
+//	// user_mileage_search_list
+//	@Query(nativeQuery = true, value = "select * from user_detail_mileage_view where user_no = ?1 order by no desc")
+//	public List<UserMileageVO> mileage_search_list_all(String user_no);
+//
+//	@Query(nativeQuery = true, value = "select * from user_detail_mileage_view where user_no = ?1 and state='T' order by no desc")
+//	public List<UserMileageVO> mileage_search_list_plus(String user_no);
+//
+//	@Query(nativeQuery = true, value = "select * from user_detail_mileage_view where user_no = ?1 and state='F' order by no desc")
+//	public List<UserMileageVO> mileage_search_list_minus(String user_no);
 
-	@Query(nativeQuery = true, value = "select * from user_detail_mileage_view where user_no = ?1 and state='F' order by no desc")
-	public List<UserMileageVO> mileage_search_list_minus(String user_no);
+	
+	
+
+
+	
+
 
 }// end class
--- src/main/java/com/rence/user/service/UserMypageSerivice.java
+++ src/main/java/com/rence/user/service/UserMypageSerivice.java
@@ -28,189 +28,247 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class UserMypageSerivice {
-	
+
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@Autowired
 	MypageRepository repository;
-	
+
 	@Autowired
 	MileageRepository mileageRepository;
-	
+
 	@Autowired
 	MyReserveRepository myReserveRepository;
-	
+
 	@Autowired
 	MyQuestionRepository myQuestionRepository;
-	
+
 	@Autowired
 	UserReviewRepository userReviewRepository;
-	
-	
+
 	public UserMypageSerivice() {
 		log.info("UserSerivice().....");
 	}
-	
-	
-	//마이페이지
+
+	// 마이페이지
 	public UserMypageVO user_mypage_select(UserVO uvo) {
 		log.info("user_mileage_search_list()....");
-		log.info("uvo: {}",uvo);
+		log.info("uvo: {}", uvo);
 		return repository.user_mypage_select(uvo.getUser_no());
 //		return repository.user_mypage_select("U1001");
 	}
-	
 
-	
-	//총마일리지 
+	// 총마일리지
 	public UserMileageVO totalMileage_selectOne(UserVO uvo) {
-		log.info("totalMileage_selectOne()...."+uvo.getUser_no());
+		log.info("totalMileage_selectOne()...." + uvo.getUser_no());
 		return mileageRepository.totalMileage_selectOne(uvo.getUser_no());
-//		return null;
 
 	}
 
-	public List<UserMileageVO> user_mileage_selectAll(UserVO uvo) {
-		log.info("user_mileage_selectAll()...."+uvo.getUser_no());
-		return mileageRepository.mileage_selectAll(uvo.getUser_no());
-		
-	}
-	
-	//마일리지상세페이지에서
-	public List<UserMileageVO> user_mileage_search_list(UserVO uvo, String searchKey) {
-		log.info("user_mileage_search_list()....");
-		log.info("uvo: {}",uvo);
-		log.info("searchKey: {}",searchKey);
-		
-//		Native
-		if(searchKey.equals("all")) {			
-			return mileageRepository.mileage_search_list_all(uvo.getUser_no());			
-		}
-		else if(searchKey.equals("plus")) {			
-			return mileageRepository.mileage_search_list_plus(uvo.getUser_no());			
-		}
-		else if(searchKey.equals("minus")) {			
-			return mileageRepository.mileage_search_list_minus(uvo.getUser_no());			
-		}
-		return null;
-		
-	}
-
-	
-	//현재 비밀번호 확인
+	// 현재 비밀번호 확인
 	public int check_now_pw(UserVO uvo) {
 		log.info("check_now_pw()....");
-		log.info("(비밀번호 확인부분)uvo: {}",uvo);
+		log.info("(비밀번호 확인부분)uvo: {}", uvo);
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
-		//평문과 비교를 하는것이기때문에 입력된값(평문)을 암호화 해서는 안됨!!!!
+
+		// 평문과 비교를 하는것이기때문에 입력된값(평문)을 암호화 해서는 안됨!!!!
 //		uvo.setUser_pw(encoder.encode(uvo.getPassword()));
 //		log.info("(비밀번호 확인부분)암호화 PW: {}",uvo.getUser_pw());
 //		log.info("(비밀번호 확인부분)암호화 uvo: {}",uvo);
-		
+
 		UserVO uvo2 = userRepository.check_now_pw_selectOne(uvo.getUser_no());
-		log.info("(비밀번호 확인부분)uvo2: {}",uvo2);
-		
-		boolean match_res = encoder.matches(uvo.getPassword(), uvo2.getPassword()); 
-		log.info("(비밀번호 확인부분)res: {}",match_res);
+		log.info("(비밀번호 확인부분)uvo2: {}", uvo2);
+
+		boolean match_res = encoder.matches(uvo.getPassword(), uvo2.getPassword());
+		log.info("(비밀번호 확인부분)res: {}", match_res);
 		int result;
-		if(match_res == true) {
+		if (match_res == true) {
 			result = 1;
-		}else {
+		} else {
 			result = 0;
 		}
-			
+
 //		 uvo.setUser_pw("$2a$12$SkMR3vslquCDjRVoFWGtI.XoN8Bs8DsWfrYqHb.jSt6IL3EgXLVeC"); //test1234!	
 		return result;
 	}
-	
-	
-	//비밀번호 변경
+
+	// 비밀번호 변경
 	public int user_pw_updateOK(UserVO uvo) {
 		log.info("user_pw_updateOK()....");
-		log.info("uvo: {}",uvo);
-		
-		//비밀번호 암호화 암호화
+		log.info("uvo: {}", uvo);
+
+		// 비밀번호 암호화 암호화
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		uvo.setUser_pw(encoder.encode(uvo.getUser_pw()));
-		
-		log.info("(비밀번호 변경부분)암호화 PW: {}",uvo.getUser_pw());
-		
-		return userRepository.user_pw_updateOK(uvo.getUser_pw(),uvo.getUser_no());
+
+		log.info("(비밀번호 변경부분)암호화 PW: {}", uvo.getUser_pw());
+
+		return userRepository.user_pw_updateOK(uvo.getUser_pw(), uvo.getUser_no());
 	}
 
-
-	//회원탈퇴에 따른 회원상태 수정
+	// 회원탈퇴에 따른 회원상태 수정
 	public int user_secedeOK(UserVO uvo) {
 		log.info("user_secedeOK()....");
-		log.info("uvo: {}",uvo);
-		
+		log.info("uvo: {}", uvo);
+
 		return repository.user_secedeOK(uvo.getUser_no());
 	}
-	
-	
-	//프로필 사진 수정
+
+	// 프로필 사진 수정
 	public int user_img_updateOK(UserVO uvo) {
 		log.info("user_img_updateOK()....");
+		log.info("uvo: {}", uvo);
+
+		return repository.user_img_updateOK(uvo.getUser_image(), uvo.getUser_no());
+	}
+	
+	// 마일리지 리스트수-현재(페이징 처리를 위해서) all
+	public long total_rowCount_mileage_all(UserVO uvo) {
+		log.info("total_rowCount_mileage_all()....");
+		return mileageRepository.count_allmileage(uvo.getUser_no());
+	}
+	
+	// 마일리지 리스트수-현재(페이징 처리를 위해서) plus/minus
+	public long total_rowCount_mileage_searchKey(UserVO uvo, String searchKey) {
+		log.info("total_rowCount_mileage_searchKey()....");
+		log.info("uvo: {}", uvo);
+		log.info("searchKey: {}", searchKey);
+		long total_rowCount_mileage_all = 0;
+//		Native
+		if (searchKey.equals("all")) {
+			total_rowCount_mileage_all = mileageRepository.count_allmileage(uvo.getUser_no());
+		} else if (searchKey.equals("plus")) {
+			total_rowCount_mileage_all = mileageRepository.count_plusmileage(uvo.getUser_no());
+		} else if (searchKey.equals("minus")) {
+			total_rowCount_mileage_all = mileageRepository.count_minusmileage(uvo.getUser_no());
+		}
+		return total_rowCount_mileage_all;	
+	}
+	
+	
+	public List<UserMileageVO> user_mileage_selectAll_paging(UserVO uvo, Integer page) {
+		log.info("user_mileage_selectAll_paging");
 		log.info("uvo: {}",uvo);
 		
-		return repository.user_img_updateOK(uvo.getUser_image(),uvo.getUser_no());
+		Integer row_count = 8;
+		Integer start_row = (page -1) * row_count + 1;
+		Integer end_row = page * row_count;
+		
+		return mileageRepository.user_mileage_selectAll_paging(uvo.getUser_no(), start_row, end_row);
 	}
 
-	//마이페이지- 현재 예약현황 리스트
+	
+	public List<UserMileageVO> user_mileage_search_list_paging(UserVO uvo, String searchKey, Integer page) {
+		log.info("user_mileage_search_list()....");
+		log.info("uvo: {}", uvo);
+		log.info("searchKey: {}", searchKey);
+		List<UserMileageVO> vos = null;
+		
+		Integer row_count = 8;
+		Integer start_row = (page -1) * row_count + 1;
+		Integer end_row = page * row_count;
+		
+		
+		if (searchKey.equals("all")) {
+			vos= mileageRepository.user_mileage_selectAll_paging(uvo.getUser_no(),start_row, end_row);
+		} else if (searchKey.equals("plus")) {
+			vos= mileageRepository.mileage_search_list_plus_paging(uvo.getUser_no(),start_row, end_row);
+		} else if (searchKey.equals("minus")) {
+			vos= mileageRepository.mileage_search_list_minus_paging(uvo.getUser_no(),start_row, end_row);
+		}
+		return vos;
+	}
+
+	
+	
+//	public List<UserMileageVO> user_mileage_selectAll(UserVO uvo) {
+//		log.info("user_mileage_selectAll()...." + uvo.getUser_no());
+//		return mileageRepository.mileage_selectAll(uvo.getUser_no());
+//
+//	}
+//
+//	// 마일리지상세페이지에서
+//	public List<UserMileageVO> user_mileage_search_list(UserVO uvo, String searchKey) {
+//		log.info("user_mileage_search_list()....");
+//		log.info("uvo: {}", uvo);
+//		log.info("searchKey: {}", searchKey);
+//
+//		//		Native
+//		if (searchKey.equals("all")) {
+//			return mileageRepository.mileage_search_list_all(uvo.getUser_no());
+//		} else if (searchKey.equals("plus")) {
+//			return mileageRepository.mileage_search_list_plus(uvo.getUser_no());
+//		} else if (searchKey.equals("minus")) {
+//			return mileageRepository.mileage_search_list_minus(uvo.getUser_no());
+//		}
+//		return null;
+//		
+//
+//	}
+
+	// 예약 리스트수-현재(페이징 처리를 위해서)
+	public long total_rowCount_reserve_now() {
+		log.info("total_rowCount_reserve_now()....");
+		return myReserveRepository.count_nowReserve();
+	}
+
+	// 마이페이지- 현재 예약현황 리스트
 	public List<MyPageReserveListVO> select_all_now_reserve_list(String user_no) {
 		log.info("select_all_now_reserve_list()....");
-		log.info("user_no: {}",user_no);
-		
+		log.info("user_no: {}", user_no);
+
 		List<MyPageReserveListVO> vos = myReserveRepository.select_all_now_reserve_list(user_no);
-		log.info("vos: {}",vos);
-		
+		log.info("vos: {}", vos);
+
 		return vos;
 	}
 
-	//마이페이지- 과거 예약현황 리스트
+	// 마이페이지- 과거 예약현황 리스트
 	public List<MyPageReserveListVO> select_all_before_reserve_list(String user_no) {
 		log.info("select_all_before_reserve_list()....");
-		log.info("user_no: {}",user_no);
-		
+		log.info("user_no: {}", user_no);
+
 		List<MyPageReserveListVO> vos = myReserveRepository.select_all_before_reserve_list(user_no);
-		log.info("vos: {}",vos);
-		
+		log.info("vos: {}", vos);
+
 		return vos;
 	}
 
-	//마이페이지 - 문의내역리스트
+	// 마이페이지 - 문의내역리스트
 	public List<UserQuestionVO> select_all_question(String user_no) {
 		log.info("select_all_question()....");
-		log.info("user_no: {}",user_no);
+		log.info("user_no: {}", user_no);
 		return myQuestionRepository.select_all_question(user_no);
 	}
 
-	//마이페이지 - 문의내역리스트
+	// 마이페이지 - 문의내역리스트
 	public UserQuestionVO select_one_answer(String comment_no) {
 		log.info("select_all_question()....");
-		log.info("comment_no: {}",comment_no);
+		log.info("comment_no: {}", comment_no);
 		return myQuestionRepository.select_one_answer(comment_no);
 	}
 
-	//마이페이지 - 문의내역리스트 - 문의 삭제
+	// 마이페이지 - 문의내역리스트 - 문의 삭제
 	public int delete_comment(String comment_no) {
 		log.info("delete_comment()....");
-		log.info("comment_no: {}",comment_no);
+		log.info("comment_no: {}", comment_no);
 		return myQuestionRepository.delete_comment(comment_no);
 //		return 0;
 	}
 
-
-	
-
-	//마이페이지 - 리뷰리스트
+	// 마이페이지 - 리뷰리스트
 	public List<UserReviewVO> select_all_review(String user_no) {
 		log.info("select_all_review()....");
-		log.info("user_no: {}",user_no);
+		log.info("user_no: {}", user_no);
 		return userReviewRepository.select_all_review(user_no);
 	}
 
+	
 
-}//end class
+	
+	
+	
+
+
+}// end class
