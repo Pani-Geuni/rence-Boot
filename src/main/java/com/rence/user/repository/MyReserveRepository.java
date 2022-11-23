bc427691b3c63dbf711a5a5baa3887a30709271e
KANGKYEONGSEOK <rudtjr1303@naver.com>
2022-11-23 09:37:37 +0900

    [feat]예약리스트 페이징 구현

--- src/main/java/com/rence/user/repository/MyReserveRepository.java
+++ src/main/java/com/rence/user/repository/MyReserveRepository.java
@@ -15,34 +15,39 @@ import com.rence.user.model.MyPageReserveListVO;
 public interface MyReserveRepository extends JpaRepository<MyPageReserveListVO,Object> {
 
 	// 예약 리스트수-현재(페이징 처리를 위해서)
-			@Query(nativeQuery = true, 
-					value= "select count(*) from USER_RESERVE_VIEW where user_no = ?1 and payment_total is not null and sysdate <= reserve_edate ")
-			public long count_nowReserve();
+	@Query(nativeQuery = true, value = "select count(*) from USER_RESERVE_VIEW where user_no = ?1 and payment_total is not null and sysdate <= reserve_edate ")
+	public long count_Reserve(String user_no, String time_point);
+
+	// 마이페이지- 현재 예약현황 리스트 - 페이징
+	@Query(nativeQuery = true, value = "select * from(select ROWNUM as rn, u.* from (select * from USER_RESERVE_VIEW where user_no = ?1 and payment_total is not null and sysdate <= reserve_edate order by reserve_no desc) u) where rn between ?2 and ?3")
+	public List<MyPageReserveListVO> select_all_now_reserve_list_paging(String user_no, Integer start_row, Integer end_row);
+
+	
+	// 마이페이지- 과거 예약현황 리스트 - 페이징
+	@Query(nativeQuery = true, value = "select * from(select ROWNUM as rn, u.* from (select * from USER_RESERVE_VIEW where user_no = ?1 and payment_total is not null and sysdate ? reserve_edate order by reserve_no desc) u) where rn between ?2 and ?3")
+	public List<MyPageReserveListVO> select_all_before_reserve_list_paging(String user_no, Integer start_row,
+			Integer end_row);
 	
 	
-	//마이페이지- 현재 예약현황 리스트 가져오기
-		@Query(nativeQuery = true, 
-				value= "select * from USER_RESERVE_VIEW where user_no = ?1 and payment_total is not null and sysdate <= reserve_edate "
-						+ "and ROWNUM between 1 and 1")
-//				value = "select leftdb.reserve_no, leftdb.user_no, leftdb.reserve_sdate, leftdb.reserve_edate, leftdb.company_name, leftdb.roadname_address, leftdb.backoffice_image, p.payment_total \r\n"
-//						+ "		from(\r\n"
-//						+ "		    select r.reserve_no, r.user_no,r.reserve_sdate, r.reserve_edate, b.company_name, b.roadname_address, b.backoffice_image\r\n"
-//						+ "		    from reserveinfo r left join backofficeinfo b \r\n"
-//						+ "		    on r.backoffice_no = b.backoffice_no\r\n"
-//						+ "		) leftdb left join paymentinfo p\r\n"
-//						+ "		on leftdb.reserve_no = p.reserve_no\r\n"
-//						+ "		where leftdb.user_no = ?1 AND p.payment_total is not null AND sysdate <= leftdb.reserve_edate")
-		public List<MyPageReserveListVO> select_all_now_reserve_list(String user_no);
+//	// 마이페이지- 현재 예약현황 리스트 가져오기
+//	@Query(nativeQuery = true, value = "select * from USER_RESERVE_VIEW where user_no = ?1 and payment_total is not null and sysdate <= reserve_edate "
+//			+ "and ROWNUM between 1 and 1")
+//	public List<MyPageReserveListVO> select_all_now_reserve_list(String user_no);
+//		
+//	// 마이페이지- 과거 예약현황 리스트
+//	@Query(nativeQuery = true, value = "select * from USER_RESERVE_VIEW where user_no = ?1 "
+//			+ "and payment_total is not null and sysdate > reserve_edate")
+//	public List<MyPageReserveListVO> select_all_before_reserve_list(String user_no);
+
+	
+
 
 		
+
+	
 		
-		
-		
-		//마이페이지- 과거 예약현황 리스트
-		@Query(nativeQuery = true, 
-				value= "select * from USER_RESERVE_VIEW where user_no = ?1 "
-						+ "and payment_total is not null and sysdate <= reserve_edate")
-		public List<MyPageReserveListVO> select_all_before_reserve_list(String user_no);
+
+
 
 
 
