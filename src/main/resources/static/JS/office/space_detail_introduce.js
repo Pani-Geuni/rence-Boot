0ea2f50bffd6b954c82c47832583b410b6def5e8
PanGeun <lifebook0809@gmail.com>
2022-11-24 20:56:40 +0900

    [refactor] CSS 수정

--- src/main/resources/static/JS/office/space_detail_introduce.js
+++ src/main/resources/static/JS/office/space_detail_introduce.js
@@ -3,7 +3,6 @@
  */
  $(function(){
   let test = 1;
-  let pickerDate = '';
 
   $("#common-alert-btn").click(function(){
     $(".popup-background:eq(1)").addClass("blind");
@@ -106,23 +105,7 @@
     $(".type-border").removeClass("open-select");
   });
 
-   
-  // ***************
-  // 선택 날짜 요일 체크
-  // ***************
-  function getDayOfWeek(date) {
-    const week = [0, 1, 2, 3, 4, 5, 6]
-    const dayOfWeek = week[new Date(date).getDay()]
-    return dayOfWeek
-  }
-
-  // 예약 날짜 미선택 시 사용 시간 display none
-  if (!pickerDate) {
-    $('#reserve-time-boundary').css('display', 'none')
-  }
-
-  // 예약 선택 시간
-  let pick_time_list = []
+  
 
   // 예약 선택 시간 boundary
   // ex) 11, 16시 선택 시
@@ -131,60 +114,12 @@
   // 중간 시간에 예약이 있으면 불가능함.
   let check_reserve_time = []
 
-  // 시간 선택
-  $('.type-border-txt.time-input').change(function () {
-    $('.time-boundary-item').removeClass('selected')
-    pick_time_list = []
-    pickerDate = $('.type-border-txt.time-input').val()
-    let dayOfWeek = getDayOfWeek(pickerDate)
-
-    // ***********************
-    // 해당 요일의 운영 시간 가져오기
-    // ***********************
-    let stime = ''
-    let etime = ''
-
-	// *************************
-	// 예약 가능한 시간대를 리스트로 표시
-	// *************************
-//    $('.running-time-li-wrap li').each(function (index, item) {
-//      if (index === dayOfWeek) {
-//        let running_time = $(this).children('span').text().trim()
-//
-//        if (running_time !== '휴무') {
-//          let split_time = running_time.split(' ~ ')
-//
-//          stime = parseInt(split_time[0].split(':')[0])
-//          etime = parseInt(split_time[1].split(':')[0])
-//
-//          // 예약 날짜 선택 시 사용 시간 보이게 하기.
-//          $('#reserve-time-boundary').css('display', 'flex')
-//          let running_time_list = []
-//          for (var t = stime; t < etime; t++) {
-//            running_time_list.push(t)
-//          }
-//
-//          // 운영 시간 display 표시
-//          $('.time-boundary-list li').each(function (index, item) {
-//            // 이전 운영 시간 display 초기화
-//            $(this).css('display', 'none')
-//            if (running_time_list.includes(index)) {
-//              $(this).css('display', 'flex')
-//            }
-//          })
-//        } else {
-//          $('.time-boundary-list li').each(function (index, item) {
-//            $(this).css('display', 'none')
-//          });
-//        }
-//      }
-//    });
-  });
 
   // pick_time_list에 시간이 하나만 들어가 있으면 1시간 대여
   // 2개 있으면 그 사이 시간을 대여하는 것.
   $('.time-boundary-item').on('click', function () {
     if ($(this).hasClass('selected')) {
+	
       // 선택 취소 했을 때
       pick_time_list.pop($(this).val());
       $(this).toggleClass('selected');
@@ -199,10 +134,12 @@
 
     // 예약 불가 처리
     if (pick_time_list.length == 2) {
-      check_reserve_time = []
+      check_reserve_time = [];
       for (var t = pick_time_list[0]; t <= pick_time_list[1]; t++) {
         check_reserve_time.push(t);
       }
+      console.log("pick_time_list");
+      console.log(check_reserve_time);
 
       // 예약 가능 유무 체크
       let flag = 0
