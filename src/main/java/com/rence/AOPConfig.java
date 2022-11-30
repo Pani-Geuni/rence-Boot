9e59de617d7543a9d69638bce1079d95c0ff400c
Choi <choetruth44@gmail.com>
2022-11-30 11:42:31 +0900

    [refactor] 시큐리티 불필요 파일 제거
    시큐리티 불필요 파일 제거

--- src/main/java/com/rence/AOPConfig.java
+++ src/main/java/com/rence/AOPConfig.java
@@ -13,23 +13,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Configuration
 public class AOPConfig {
-
-//	@Bean
-//	public ReserveAutoUpdateAspect testPointcutAspect() {
-//		log.info("ReserveAutoUpdateAspect()...");
-//		return new ReserveAutoUpdateAspect();
-//	}
 	
 	@Bean
 	public AutoAspect autoAspect() {
 		log.info("autoAspect()...");
 		return new AutoAspect();
 	}
-	
-//	@Bean
-//	public AutoAspect authDelete() {
-//		log.info("authDelete()...");
-//		return new AutoAspect();
-//	}
 	
 }
