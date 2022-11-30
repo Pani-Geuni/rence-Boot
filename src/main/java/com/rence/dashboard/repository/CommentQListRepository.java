e8b1d03e2342ed65b9745b34a6ed5ea2495e11f3
Choi <choetruth44@gmail.com>
2022-11-30 10:56:13 +0900

    [refactor] 문의 리스트 출력 정렬 수정
    문의 리스트 출력 정렬 수정

--- src/main/java/com/rence/dashboard/repository/CommentQListRepository.java
+++ src/main/java/com/rence/dashboard/repository/CommentQListRepository.java
@@ -18,7 +18,7 @@ public interface CommentQListRepository extends JpaRepository<CommentListQView, 
 	// 공간 - 문의 리스트
 //	public List<CommentListQView> backoffice_qna_selectAll(String backoffice_no);
 
-	@Query(nativeQuery = true, value = "select * from (select rownum as rnum, comment_no, user_no, comment_content, comment_date, room_name, comment_state, room_no, backoffice_no FROM COMMENTLIST_Q_VIEW WHERE BACKOFFICE_NO=?1 order by comment_date desc) where rnum between ?2 and ?3")
+	@Query(nativeQuery = true, value = "select * from (select rownum as rnum,C.* from(select comment_no, user_no, comment_content, comment_date, room_name, comment_state, room_no, backoffice_no FROM COMMENTLIST_Q_VIEW WHERE BACKOFFICE_NO=?1 order by comment_date desc )C ) where rnum between ?2 and ?3")
 	public List<CommentListQView> select_all_q(String backoffice_no,Integer start_row, Integer end_row);
 	
 	// 문의 리스트 갯수
