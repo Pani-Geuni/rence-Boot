2dbc034618a6b9dbd3511a7f632f432236cb2aae
KANGKYEONGSEOK <rudtjr1303@naver.com>
2022-12-02 15:41:27 +0900

    [refactor] 불필요부분 삭제 수정코드 재업로드

--- src/main/java/com/rence/dashboard/model/ReserveListView.java
+++ src/main/java/com/rence/dashboard/model/ReserveListView.java
@@ -16,52 +16,49 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Entity
 @Immutable
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name="RESERVE_LIST_B_VIEW")
-
 public class ReserveListView {
-	
-	@Id
-	@Column(name="reserve_no")
-	private String reserve_no;
-	
-	@Column(name="reserve_sdate", insertable= false, updatable = false)
-	private String reserve_sdate;
-	
-	@Column(name="reserve_edate", insertable= false, updatable = false)
-	private String reserve_edate;
-	
-	@Column(name="reserve_state")
-	private String reserve_state;
-	
-	@Column(name="user_no")
-	private String user_no;
-	
-	@Column(name="backoffice_no")
-	private String backoffice_no;
-	
-	@Column(name="room_name")
-	private String room_name;
-	
-	@Column(name="user_name")
-	private String user_name;
-	
-	@Column(name="user_email")
-	private String user_email;
-	
-	@Column(name="user_tel")
-	private String user_tel;
-	
-	@Column(name="actual_payment")
-	private String actual_payment;
-	
-	@Column(name="payment_state")
-	private String payment_state;
+   
+   @Id
+   @Column(name="reserve_no")
+   private String reserve_no;
+   
+   @Column(name="reserve_sdate", insertable= false, updatable = false)
+   private String reserve_sdate;
+   
+   @Column(name="reserve_edate", insertable= false, updatable = false)
+   private String reserve_edate;
+   
+   @Column(name="reserve_state")
+   private String reserve_state;
+   
+   @Column(name="user_no")
+   private String user_no;
+   
+   @Column(name="backoffice_no")
+   private String backoffice_no;
+   
+   @Column(name="room_name")
+   private String room_name;
+   
+   @Column(name="user_name")
+   private String user_name;
+   
+   @Column(name="user_email")
+   private String user_email;
+   
+   @Column(name="user_tel")
+   private String user_tel;
+   
+   @Column(name="actual_payment")
+   private String actual_payment;
+   
+   @Column(name="payment_state")
+   private String payment_state;
 
 }
