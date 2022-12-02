365d9ad41c5e15a7f2077da0ff7b8c9ab14ee4bc
wtw <qjspdl@hs.ac.kr>
2022-12-02 09:28:57 +0900

    [refactor] 예약 리스트 페이징 수정

--- src/main/resources/static/JS/backoffice/reserve_list.js
+++ src/main/resources/static/JS/backoffice/reserve_list.js
@@ -54,15 +54,21 @@ $(function() {
 	});
 
 
-	$(".paging-num-wrap").on("click", ".paging-box.paging-num", function(){
+	$(".reserve_search_paging").on("click", ".paging-box.paging-num", function(){
         var backoffice_no = window.location.href.split("?backoffice_no=")[1].split("&")[0];
         var reserve_state = window.location.href.split("&reserve_state=")[1].split("&")[0];
 
-        window.location.href = "/backoffice/reserve?backoffice_no=" + backoffice_no + "&reserve_state=" + reserve_state + "&page=" + $(this).attr("idx");
+		if(window.location.href.includes("/backoffice/reserve?")){
+	        window.location.href = "/backoffice/reserve?backoffice_no=" + backoffice_no + "&reserve_state=" + reserve_state + "&page=" + $(this).attr("idx");
+		}else{
+	        var searchword = window.location.href.split("&searchword=")[1].split("&")[0];
+	        window.location.href = "/backoffice/search_reserve?backoffice_no=" + backoffice_no + "&reserve_state=" + reserve_state + "&searchword=" + searchword + "&page=" + $(this).attr("idx");
+			
+		}
     });
     
     // 다음 페이지 리스트로 이동
-    $(".next-page-btn").click(function(){
+    $(".reserve_search_paging").on("click", ".next-page-btn", function(){
 		var start = Number($($(".paging-box.paging-num")[0]).text()) + 5;
 		var last = Number($($(".paging-box.paging-num")[4]).text()) + 5;
 		var totalPageCnt = Number($("#totalPageCnt").val());
@@ -92,24 +98,24 @@ $(function() {
 	});
 	
     // 이전 페이지 리스트로 이동
-    $(".before-page-btn").click(function(){
-		var start = Number($($(".paging-box.paging-num")[0]).text()) - 5;
-		var last = Number($(".paging-box.paging-num:last").text()) - 5;
+	$(".reserve_search_paging").on("click", ".before-page-btn", function(){
+		var start = Number($($(".reserve_search_paging").find(".paging-box.paging-num")[0]).text()) - 5;
+		var last = Number($(".reserve_search_paging").find(".paging-box.paging-num:last").text()) - 5;
 		
 		if(last % 5 != 0){
 			last += 5 - (last % 5);
 		}
 		
-		if($(".next-page-btn").hasClass("hide")){
-			$(".next-page-btn").removeClass("hide");
+		if($(".reserve_search_paging").find(".next-page-btn").hasClass("hide")){
+			$(".reserve_search_paging").find(".next-page-btn").removeClass("hide");
 		}
 		
 		if(start == 1){
-			$(".before-page-btn").addClass("hide");
+			$(".reserve_search_paging").find(".before-page-btn").addClass("hide");
 		}
 		
-		var sample = $(".paging-num-wrap>.paging-box.paging-num:eq(0)").clone();
-		$(".paging-num-wrap").empty();
+		var sample = $(".reserve_search_paging").find(".paging-num-wrap>.paging-box.paging-num:eq(0)").clone();
+		$(".reserve_search_paging").find(".paging-num-wrap").empty();
 		
 		for(var i = start; i <= last; i++){
 			var sample_span = sample.clone();
@@ -119,7 +125,7 @@ $(function() {
 			sample_span.removeClass("choice");
 			sample_span.addClass("un-choice");
 			
-			$(".paging-num-wrap").append(sample_span);
+			$(".reserve_search_paging").find(".paging-num-wrap").append(sample_span);
 		}
 	});
 
